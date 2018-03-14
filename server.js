613e0e3859f6c2c15cf44147194cab4c70e50a95
Mark Alaniz <markalaniz000@gmail.com>
2018-03-14 15:07:43 -0500

    added connection for mlab on heroku and reduced folder clutter

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
 // Dependencies
-var mongoose = require('mongoose');
+var mongoose = require("mongoose");
 var express = require("express");
 var bodyParser = require("body-parser");
-// var axios = require('axios');
 
 // Require all models
-var db = require('./models');
+var db = require("./models");
 
 // Set up express app
 var app = express();
@@ -17,19 +16,20 @@ app.use(bodyParser.json());
 
 // Access static directory
 app.use(express.static(__dirname + "/public"));
-
+// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/runescrape";
 // Connect to mongo
 mongoose.Promise = Promise;
-mongoose.connect('mongodb://localhost/runescrape');
+mongoose.connect(MONGODB_URI);
 // Set Handlebars
 var exphbs = require("express-handlebars");
-app.engine("handlebars", exphbs({defaultLayout: "main"}));
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 //Routes
-require('./routes/api.js')(app);
-require('./routes/index.js')(app);
-require('./routes/view.js')(app);
+require("./routes/api.js")(app);
+require("./routes/index.js")(app);
+require("./routes/view.js")(app);
 
 // Start the server
 app.listen(PORT, function() {
